d06c04ffe894a0ec4ab975ff4d653d9fff242270
Tobias Reich <tobias.reich.ich@gmail.com>
2014-03-16 13:17:29 +0100

    Close upload on error

--- assets/js/lychee.js
+++ assets/js/lychee.js
@@ -102,6 +102,7 @@ var lychee = {
 			error: function(jqXHR, textStatus, errorThrown) {
 
 				lychee.error("Server error or API not found.", params, errorThrown);
+				upload.close(true);
 
 			}
 		});
