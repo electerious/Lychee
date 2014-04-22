6a3c0c72ebd86c6163e4d028029e5fb70d95518e
Tobias Reich <tobias.reich.ich@gmail.com>
2014-04-22 11:47:39 +0200

    Added LYCHEE_UPLOADS

--- php/define.php
+++ php/define.php
@@ -11,9 +11,10 @@ define('LYCHEE', substr(__DIR__, 0, -3));
 
 # Define dirs
 define('LYCHEE_DATA', LYCHEE . 'data/');
-define('LYCHEE_UPLOADS_BIG', LYCHEE . 'uploads/big/');
-define('LYCHEE_UPLOADS_THUMB', LYCHEE . 'uploads/thumb/');
-define('LYCHEE_UPLOADS_IMPORT', LYCHEE . 'uploads/import/');
+define('LYCHEE_UPLOADS', LYCHEE . 'uploads/');
+define('LYCHEE_UPLOADS_BIG', LYCHEE_UPLOADS . 'big/');
+define('LYCHEE_UPLOADS_THUMB', LYCHEE_UPLOADS . 'thumb/');
+define('LYCHEE_UPLOADS_IMPORT', LYCHEE_UPLOADS . 'import/');
 define('LYCHEE_PLUGINS', LYCHEE . 'plugins/');
 
 # Define files
