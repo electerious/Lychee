39131033db60c44a031b7db2945f751f54f7eabb
Tobias Reich <tobias.reich.ich@gmail.com>
2014-05-06 22:11:37 +0200

    Added notice and warning function to Log (#143)

--- php/modules/Log.php
+++ php/modules/Log.php
@@ -10,25 +10,44 @@ if (!defined('LYCHEE')) exit('Error: Direct access is not allowed!');
 
 class Log extends Module {
 
+	public static function notice($database, $function, $line, $text = '') {
+
+		return Log::text($database, 'notice', $function, $line, $text);
+
+	}
+
+	public static function warning($database, $function, $line, $text = '') {
+
+		return Log::text($database, 'warning', $function, $line, $text);
+
+	}
+
 	public static function error($database, $function, $line, $text = '') {
 
+		return Log::text($database, 'error', $function, $line, $text);
+
+	}
+
+	public static function text($database, $type, $function, $line, $text = '') {
+
 		# Check dependencies
-		Module::dependencies(isset($database, $function, $line, $text));
+		Module::dependencies(isset($database, $type, $function, $line, $text));
 
 		# Get time
 		$sysstamp = time();
 
 		# Escape
+		$type		= mysqli_real_escape_string($type, $function);
 		$function	= mysqli_real_escape_string($database, $function);
 		$line		= mysqli_real_escape_string($database, $line);
 		$text		= mysqli_real_escape_string($database, $text);
 
 		# Save in database
-		$query	= "INSERT INTO lychee_log (time, type, function, line, text) VALUES ('$sysstamp', 'error', '$function', '$line', '$text');";
+		$query	= "INSERT INTO lychee_log (time, type, function, line, text) VALUES ('$sysstamp', '$type', '$function', '$line', '$text');";
 		$result	= $database->query($query);
 
 		if (!$result) return false;
-		return $database->insert_id;
+		return true;
 
 	}
 
