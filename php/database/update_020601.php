a613ee58c79dfbbe39440c3c85c8948e502a7e8d
Tobias Reich <tobias.reich.ich@gmail.com>
2014-08-22 20:52:23 +0200

    Default ist downloadable

--- php/database/update_020601.php
+++ php/database/update_020601.php
@@ -8,7 +8,7 @@
 
 # Add `downloadable`
 if (!$database->query("SELECT `downloadable` FROM `lychee_albums` LIMIT 1;")) {
-	$result = $database->query("ALTER TABLE `lychee_albums` ADD `downloadable` TINYINT(1) NOT NULL DEFAULT 0");
+	$result = $database->query("ALTER TABLE `lychee_albums` ADD `downloadable` TINYINT(1) NOT NULL DEFAULT 1");
 	if (!$result) {
 		Log::error($database, 'update_020601', __LINE__, 'Could not update database (' . $database->error . ')');
 		return false;
