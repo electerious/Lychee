c90b5f2d99b74c3f48f43edf1742db2ebf23da7f
Tobias Reich <tobias.reich.ich@gmail.com>
2014-08-24 22:10:21 +0200

    Added select all

--- assets/js/multiselect.js
+++ assets/js/multiselect.js
@@ -9,28 +9,65 @@ multiselect = {
 
 	position: {
 
-		top: null,
-		right: null,
-		bottom: null,
-		left: null
+		top:	null,
+		right:	null,
+		bottom:	null,
+		left:	null
 
 	},
 
 	show: function(e) {
 
-		if (mobileBrowser()) return false;
-		if (lychee.publicMode) return false;
-		if (visible.search()) return false;
-		if ($('.album:hover, .photo:hover').length!==0) return false;
-		if (visible.multiselect()) $('#multiselect').remove();
+		if (mobileBrowser())	return false;
+		if (lychee.publicMode)	return false;
+		if (visible.search())	return false;
+		if (visible.infobox())	return false;
+		if (!visible.albums()&&!visible.album)			return false;
+		if ($('.album:hover, .photo:hover').length!==0)	return false;
+		if (visible.multiselect())						$('#multiselect').remove();
 
-		multiselect.position.top = e.pageY;
-		multiselect.position.right = -1 * (e.pageX - $(document).width());
-		multiselect.position.bottom = -1 * (multiselect.position.top - $(window).height());
-		multiselect.position.left = e.pageX;
+		multiselect.position.top	= e.pageY;
+		multiselect.position.right	= -1 * (e.pageX - $(document).width());
+		multiselect.position.bottom	= -1 * (multiselect.position.top - $(window).height());
+		multiselect.position.left	= e.pageX;
 
 		$('body').append(build.multiselect(multiselect.position.top, multiselect.position.left));
 		$(document).on('mousemove', multiselect.resize);
+
+	},
+
+	selectAll: function() {
+
+		var e;
+
+		if (mobileBrowser())		return false;
+		if (lychee.publicMode)		return false;
+		if (visible.search())		return false;
+		if (visible.infobox())		return false;
+		if (!visible.albums()&&!visible.album)	return false;
+		if (visible.multiselect())	$('#multiselect').remove();
+
+		multiselect.position.top	= 70;
+		multiselect.position.right	= 40;
+		multiselect.position.bottom	= 90;
+		multiselect.position.left	= 20;
+
+		$('body').append(build.multiselect(multiselect.position.top, multiselect.position.left));
+
+		newWidth	= $(document).width() - multiselect.position.right + 2;
+		newHeight	= $(document).height() - multiselect.position.bottom;
+
+		$('#multiselect').css({
+			width: newWidth,
+			height: newHeight
+		});
+
+		e = {
+			pageX: $(document).width() - (multiselect.position.right / 2),
+			pageY: $(document).height() - multiselect.position.bottom
+		}
+
+		multiselect.getSelection(e);
 
 	},
 
@@ -49,7 +86,7 @@ multiselect = {
 		if (mouse_y>=multiselect.position.top) {
 
 			// Do not leave the screen
-			newHeight = e.pageY - multiselect.position.top;
+			newHeight = mouse_y - multiselect.position.top;
 			if ((multiselect.position.top+newHeight)>=$(document).height())
 				newHeight -= (multiselect.position.top + newHeight) - $(document).height() + 2;
 
@@ -72,7 +109,7 @@ multiselect = {
 		if (mouse_x>=multiselect.position.left) {
 
 			// Do not leave the screen
-			newWidth = e.pageX - multiselect.position.left;
+			newWidth = mouse_x - multiselect.position.left;
 			if ((multiselect.position.left+newWidth)>=$(document).width())
 				newWidth -= (multiselect.position.left + newWidth) - $(document).width() + 2;
 
@@ -105,10 +142,10 @@ multiselect = {
 		if (!visible.multiselect()) return false;
 
 		return {
-			top: $('#multiselect').offset().top,
-			left: $('#multiselect').offset().left,
-			width: parseInt($('#multiselect').css('width').replace('px', '')),
-			height: parseInt($('#multiselect').css('height').replace('px', ''))
+			top:	$('#multiselect').offset().top,
+			left:	$('#multiselect').offset().left,
+			width:	parseInt($('#multiselect').css('width').replace('px', '')),
+			height:	parseInt($('#multiselect').css('height').replace('px', ''))
 		};
 
 	},
@@ -156,10 +193,10 @@ multiselect = {
 
 		multiselect.stopResize();
 
-		multiselect.position.top = null;
-		multiselect.position.right = null;
-		multiselect.position.bottom = null;
-		multiselect.position.left = null;
+		multiselect.position.top	= null;
+		multiselect.position.right	= null;
+		multiselect.position.bottom	= null;
+		multiselect.position.left	= null;
 
 		lychee.animate('#multiselect', 'fadeOut');
 		setTimeout(function() {
