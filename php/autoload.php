fc4aebae9800236928e8da5a25e0d9daba50972d
Tobias Reich <tobias.reich.ich@gmail.com>
2014-10-09 18:37:03 +0200

    Check filename before including for security reasons

--- php/autoload.php
+++ php/autoload.php
@@ -10,12 +10,18 @@ if (!defined('LYCHEE')) exit('Error: Direct access is not allowed!');
 
 function lycheeAutoloaderModules($class_name) {
 
+	$modules = array('Album', 'Database', 'Import', 'Log', 'Module', 'Photo', 'Plugins', 'Session', 'Settings');
+	if (!in_array($class_name, $modules)) return false;
+
 	$file = LYCHEE . 'php/modules/' . $class_name . '.php';
 	if (file_exists($file)!==false) require $file;
 
 }
 
 function lycheeAutoloaderAccess($class_name) {
+
+	$access = array('Access', 'Admin', 'Guest', 'Installation');
+	if (!in_array($class_name, $access)) return false;
 
 	$file = LYCHEE . 'php/access/' . $class_name . '.php';
 	if (file_exists($file)!==false) require $file;
