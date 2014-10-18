fbea4b4dacd47f96a1058d416ce3ca29c2b4f643
Tobias Reich <tobias.reich.ich@gmail.com>
2014-10-18 18:02:06 +0200

    Code adjustments

--- src/scripts/visible.js
+++ src/scripts/visible.js
@@ -5,63 +5,61 @@
  * @copyright	2014 by Tobias Reich
  */
 
-visible = {
+visible = {};
 
-	albums: function() {
-		if ($('#tools_albums').css('display')==='block') return true;
-		else return false;
-	},
+visible.albums = function() {
+	if ($('#tools_albums').css('display')==='block') return true;
+	else return false;
+},
 
-	album: function() {
-		if ($('#tools_album').css('display')==='block') return true;
-		else return false;
-	},
+visible.album = function() {
+	if ($('#tools_album').css('display')==='block') return true;
+	else return false;
+},
 
-	photo: function() {
-		if ($('#imageview.fadeIn').length>0) return true;
-		else return false;
-	},
+visible.photo = function() {
+	if ($('#imageview.fadeIn').length>0) return true;
+	else return false;
+},
 
-	search: function() {
-		if (search.code!==null&&search.code!=='') return true;
-		else return false;
-	},
+visible.search = function() {
+	if (search.code!==null&&search.code!=='') return true;
+	else return false;
+},
 
-	infobox: function() {
-		if ($('#infobox.active').length>0) return true;
-		else return false;
-	},
+visible.infobox = function() {
+	if ($('#infobox.active').length>0) return true;
+	else return false;
+},
 
-	infoboxbutton: function() {
-		if (visible.albums()) return false;
-		if (visible.photo()) return true;
-		if (visible.album()&&$('#button_info_album:visible').length>0) return true;
-		else return false;
-	},
+visible.infoboxbutton = function() {
+	if (visible.albums()) return false;
+	if (visible.photo()) return true;
+	if (visible.album()&&$('#button_info_album:visible').length>0) return true;
+	else return false;
+},
 
-	controls: function() {
-		if (lychee.loadingBar.css('opacity')<1) return false;
-		else return true;
-	},
+visible.controls = function() {
+	if (lychee.loadingBar.css('opacity')<1) return false;
+	else return true;
+},
 
-	message: function() {
-		if ($('.message').length>0) return true;
-		else return false;
-	},
+visible.message = function() {
+	if ($('.message').length>0) return true;
+	else return false;
+},
 
-	signin: function() {
-		if ($('.message .sign_in').length>0) return true;
-		else return false;
-	},
+visible.signin = function() {
+	if ($('.message .sign_in').length>0) return true;
+	else return false;
+},
 
-	contextMenu: function() {
-		if ($('.contextmenu').length>0) return true;
-		else return false;
-	},
+visible.contextMenu = function() {
+	if ($('.contextmenu').length>0) return true;
+	else return false;
+},
 
-	multiselect: function() {
-		if ($('#multiselect').length>0) return true;
-		else return false;
-	}
-
-};
+visible.multiselect = function() {
+	if ($('#multiselect').length>0) return true;
+	else return false;
+}
