f6875fc9cce8766c53b55a5f315cfb316d305058
Tobias Reich <tobias.reich.ich@gmail.com>
2014-11-06 21:31:21 +0100

    Added comments to clarify the function #258

--- php/modules/misc.php
+++ php/modules/misc.php
@@ -121,6 +121,8 @@ function get_hashed_password($password) {
 
 function hasPermissions($path) {
 
+	// Check if the given path is readable and writable
+	// Both functions are also verifying that the path exists
 	if (is_readable($path)===true&&
 		is_writeable($path)===true) return true;
 
