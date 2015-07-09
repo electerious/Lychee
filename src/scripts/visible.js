6118ad5c4a8ba6de2fa7cc968868b6ba48fbbe73
Tobias Reich <tobias.reich.ich@gmail.com>
2015-07-09 15:20:41 +0200

    Code adjustment

--- src/scripts/visible.js
+++ src/scripts/visible.js
@@ -6,57 +6,57 @@
 visible = {}
 
 visible.albums = function() {
-	if ($('#tools_albums').css('display')==='block') return true;
-	return false;
+	if ($('#tools_albums').css('display')==='block') return true
+	return false
 }
 
 visible.album = function() {
-	if ($('#tools_album').css('display')==='block') return true;
-	return false;
+	if ($('#tools_album').css('display')==='block') return true
+	return false
 }
 
 visible.photo = function() {
-	if ($('#imageview.fadeIn').length>0) return true;
-	return false;
+	if ($('#imageview.fadeIn').length>0) return true
+	return false
 }
 
 visible.search = function() {
-	if (search.hash!==null) return true;
-	return false;
+	if (search.hash!==null) return true
+	return false
 }
 
 visible.sidebar = function() {
-	if (sidebar.dom().hasClass('active')===true) return true;
-	return false;
+	if (sidebar.dom().hasClass('active')===true) return true
+	return false
 }
 
 visible.sidebarbutton = function() {
-	if (visible.albums()) return false;
-	if (visible.photo()) return true;
-	if (visible.album()&&$('#button_info_album:visible').length>0) return true;
-	return false;
+	if (visible.albums()) return false
+	if (visible.photo()) return true
+	if (visible.album() && $('#button_info_album:visible').length>0) return true
+	return false
 }
 
 visible.header = function() {
-	if (header.dom().hasClass('hidden')===true) return false;
-	return true;
+	if (header.dom().hasClass('hidden')===true) return false
+	return true
 }
 
 visible.message = function() {
-	if ($('.message').length>0) return true;
-	return false;
+	if ($('.message').length>0) return true
+	return false
 }
 
 visible.signin = function() {
-	if ($('.message .sign_in').length>0) return true;
-	return false;
+	if ($('.message .sign_in').length>0) return true
+	return false
 }
 
 visible.contextMenu = function() {
-	return basicContext.visible();
+	return basicContext.visible()
 }
 
 visible.multiselect = function() {
-	if ($('#multiselect').length>0) return true;
-	return false;
+	if ($('#multiselect').length>0) return true
+	return false
 }
