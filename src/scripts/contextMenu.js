63b2a2412d4ec21f00b0f220e12c1554c07a8b85
Tobias Reich <tobias.reich.ich@gmail.com>
2015-07-11 14:52:38 +0200

    Updated loadingBar.js (ES2015)

--- src/scripts/contextMenu.js
+++ src/scripts/contextMenu.js
@@ -1,6 +1,6 @@
 /**
- * @description	This module is used for the context menu.
- * @copyright	2015 by Tobias Reich
+ * @description This module is used for the context menu.
+ * @copyright   2015 by Tobias Reich
  */
 
 contextMenu = {}
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,6 +1,6 @@
 /**
- * @description	This module takes care of the header.
- * @copyright	2015 by Tobias Reich
+ * @description This module takes care of the header.
+ * @copyright   2015 by Tobias Reich
  */
 
 header = {
