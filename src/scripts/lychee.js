050be351e495b63658e0c0f2063ee2c0877235cc
Tobias Reich <tobias.reich.ich@gmail.com>
2015-07-12 14:05:14 +0200

    Updated version

--- src/scripts/lychee.js
+++ src/scripts/lychee.js
@@ -6,8 +6,8 @@
 lychee = {
 
 	title           : document.title,
-	version         : '3.0.3',
-	version_code    : '030003',
+	version         : '3.0.4',
+	version_code    : '030004',
 
 	update_path     : 'http://lychee.electerious.com/version/index.php',
 	updateURL       : 'https://github.com/electerious/Lychee',
