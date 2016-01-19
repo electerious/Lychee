7bce954c8d3ac3f99d512742214ed1c4a283776e
Tobias Reich <tobias.reich.ich@gmail.com>
2016-01-19 15:03:50 +0100

    Register autoloader functions as anonymous functions
    Requires PHP 5.3

--- php/autoload.php
+++ php/autoload.php
@@ -7,23 +7,20 @@
 
 if (!defined('LYCHEE')) exit('Error: Direct access is not allowed!');
 
-function lycheeAutoloaderModules($class) {
+spl_autoload_register(function($class) {
 
 	$file = LYCHEE . 'php/modules/' . $class . '.php';
 
 	if (file_exists($file)===true) require $file;
 
-}
+});
 
-function lycheeAutoloaderAccess($class) {
+spl_autoload_register(function($class) {
 
 	$file = LYCHEE . 'php/access/' . $class . '.php';
 
 	if (file_exists($file)===true) require $file;
 
-}
-
-spl_autoload_register('lycheeAutoloaderModules');
-spl_autoload_register('lycheeAutoloaderAccess');
+});
 
 ?>
