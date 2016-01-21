11768d361f54696578b936aba6e23a1397c1083e
Mike <mike.michel@mmbash.de>
2016-01-21 16:06:28 +0100

    adding Docker and docker-compose.yml

---
+++ docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+if [ ! -e '/var/www/html/view.php' ]; then
+        echo >&2 "Piwik not found in $(pwd) - copying now..."
+        		if [ "$(ls -A)" ]; then
+			         echo >&2 "WARNING: $(pwd) is not empty - press Ctrl+C now if this is an error!"
+			     ( set -x; ls -A; sleep 10 )
+                fi
+	tar cf - --one-file-system -C /usr/src/Lychee* . | tar xf -
+	chown -R www-data /var/www/html
+    echo >&2 "Complete! Piwik has been successfully copied to $(pwd)"
+fi
+
+chfn -f 'Piwik Admin' www-data
+
+exec "$@"
