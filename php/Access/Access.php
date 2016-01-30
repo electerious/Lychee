207e98a81ccbe8bed2a46b7f34c024758585e21d
Tobias Reich <tobias.reich.ich@gmail.com>
2016-01-31 00:56:42 +0100

    fnNotFound was not callable from extending class

--- php/Access/Access.php
+++ php/Access/Access.php
@@ -4,7 +4,7 @@ namespace Lychee\Access;
 
 abstract class Access {
 
-	final private static function fnNotFound() {
+	final protected static function fnNotFound() {
 
 		exit('Error: Function not found! Please check the spelling of the called function.');
 
