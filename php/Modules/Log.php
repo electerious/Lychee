9a1e51c8bd4c40965ce86446a7afcf15986be62a
Tobias Reich <tobias.reich.ich@gmail.com>
2016-02-08 22:11:39 +0100

    Added partial php documentation to the Log class

--- php/Modules/Log.php
+++ php/Modules/Log.php
@@ -4,18 +4,27 @@ namespace Lychee\Modules;
 
 final class Log {
 
+	/**
+	 * @return boolean Returns true when successful.
+	 */
 	public static function notice($connection, $function, $line, $text = '') {
 
 		return Log::text($connection, 'notice', $function, $line, $text);
 
 	}
 
+	/**
+	 * @return boolean Returns true when successful.
+	 */
 	public static function error($connection, $function, $line, $text = '') {
 
 		return Log::text($connection, 'error', $function, $line, $text);
 
 	}
 
+	/**
+	 * @return boolean Returns true when successful.
+	 */
 	private static function text($connection, $type, $function, $line, $text = '') {
 
 		// Check dependencies
