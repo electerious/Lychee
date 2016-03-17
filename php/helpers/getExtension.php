aba1629f317c5fc5669afa792955956c57626a15
Tobias Reich <tobias.reich.ich@gmail.com>
2016-03-17 09:28:31 +0100

    Syntax adjustments #488

--- php/helpers/getExtension.php
+++ php/helpers/getExtension.php
@@ -1,18 +1,20 @@
 <?php
 
-# Returns the extension of the filename (path or URI) or an empty string
+/**
+ * Returns the extension of the filename (path or URI) or an empty string.
+ * @return string Extension of the filename starting with a dot.
+ */
 function getExtension($filename, $isURI = false) {
 
-    # If $filename is an URI, get only the path component
-    if ($isURI)
-        $filename = parse_url($filename, PHP_URL_PATH);
+	// If $filename is an URI, get only the path component
+	if ($isURI===true) $filename = parse_url($filename, PHP_URL_PATH);
 
 	$extension = pathinfo($filename, PATHINFO_EXTENSION);
 
-    if (!empty($extension))
-        $extension = '.' . $extension;
+	if (empty($extension)===false) $extension = '.' . $extension;
 
 	return $extension;
+
 }
 
 ?>
