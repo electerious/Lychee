fbab70f244665a1a216731ce4aea4d9f8b233cd8
Tobias Reich <tobias.reich.ich@gmail.com>
2016-03-18 22:17:41 +0100

    Allow URL import of photos containing a question mark

--- php/helpers/getExtension.php
+++ php/helpers/getExtension.php
@@ -14,6 +14,7 @@ function getExtension($filename, $isURI = false) {
 	// Special cases
 	// https://github.com/electerious/Lychee/issues/482
 	list($extension) = explode(':', $extension, 2);
+	list($extension) = explode('?', $extension, 2);
 
 	if (empty($extension)===false) $extension = '.' . $extension;
 
