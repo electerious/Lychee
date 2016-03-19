082d306927949782576ebd99af147554370e596d
Tobias Reich <tobias.reich.ich@gmail.com>
2016-03-19 16:16:41 +0100

    Small adjustments to #491

--- php/Modules/Session.php
+++ php/Modules/Session.php
@@ -71,14 +71,14 @@ final class Session {
 		Plugins::get()->activate(__METHOD__, 0, func_get_args());
 
 		$username_crypt = crypt($username, Settings::get()['username']);
-		$password = crypt($password, Settings::get()['password']);
+		$password_crypt = crypt($password, Settings::get()['password']);
 
 		// Check login with crypted hash
 		if (Settings::get()['username']===$username_crypt&&
-			Settings::get()['password']===$password) {
+			Settings::get()['password']===$password_crypt) {
 				$_SESSION['login']      = true;
 				$_SESSION['identifier'] = Settings::get()['identifier'];
-				Log::notice(Database::get(), __METHOD__, __LINE__, 'User ['.$username.'] has logged in from ['.$_SERVER['REMOTE_ADDR'].']');
+				Log::notice(Database::get(), __METHOD__, __LINE__, 'User (' . $username . ') has logged in from ' . $_SERVER['REMOTE_ADDR']);
 				return true;
 		}
 
@@ -88,7 +88,9 @@ final class Session {
 		// Call plugins
 		Plugins::get()->activate(__METHOD__, 1, func_get_args());
 
-		Log::error(Database::get(), __METHOD__, __LINE__, 'User ['.$username.'] has tried to log in from ['.$_SERVER['REMOTE_ADDR'].']');
+		// Log failed log in
+		Log::error(Database::get(), __METHOD__, __LINE__, 'User (' . $username . ') has tried to log in from ' . $_SERVER['REMOTE_ADDR']);
+
 		return false;
 
 	}
