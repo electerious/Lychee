4dd2155326edf33b509777e8aefcb814841541e4
Tobias Reich <tobias.reich.ich@gmail.com>
2016-04-30 15:32:41 +0200

    Updated version to 3.1.1

--- src/scripts/lychee.js
+++ src/scripts/lychee.js
@@ -6,8 +6,8 @@
 lychee = {
 
 	title           : document.title,
-	version         : '3.1.0',
-	versionCode     : '030100',
+	version         : '3.1.1',
+	versionCode     : '030101',
 
 	updatePath      : '//update.electerious.com/index.json',
 	updateURL       : 'https://github.com/electerious/Lychee',
