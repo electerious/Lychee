84212e87f7baffc27868b3b135cec74d8e47760e
Patrick <patmagauran.j@gmail.com>
2016-06-19 13:14:07 -0400

    Changing stuff modifies file

--- php/modules/Photo.php
+++ php/modules/Photo.php
@@ -6,6 +6,132 @@
 ###
 
 if (!defined('LYCHEE')) exit('Error: Direct access is not allowed!');
+define("IPTC_OBJECT_NAME", "005");
+define("IPTC_EDIT_STATUS", "007");
+define("IPTC_PRIORITY", "010");
+define("IPTC_CATEGORY", "015");
+define("IPTC_SUPPLEMENTAL_CATEGORY", "020");
+define("IPTC_FIXTURE_IDENTIFIER", "022");
+define("IPTC_KEYWORDS", "025");
+define("IPTC_RELEASE_DATE", "030");
+define("IPTC_RELEASE_TIME", "035");
+define("IPTC_SPECIAL_INSTRUCTIONS", "040");
+define("IPTC_REFERENCE_SERVICE", "045");
+define("IPTC_REFERENCE_DATE", "047");
+define("IPTC_REFERENCE_NUMBER", "050");
+define("IPTC_CREATED_DATE", "055");
+define("IPTC_CREATED_TIME", "060");
+define("IPTC_ORIGINATING_PROGRAM", "065");
+define("IPTC_PROGRAM_VERSION", "070");
+define("IPTC_OBJECT_CYCLE", "075");
+define("IPTC_BYLINE", "080");
+define("IPTC_BYLINE_TITLE", "085");
+define("IPTC_CITY", "090");
+define("IPTC_PROVINCE_STATE", "095");
+define("IPTC_COUNTRY_CODE", "100");
+define("IPTC_COUNTRY", "101");
+define("IPTC_ORIGINAL_TRANSMISSION_REFERENCE", "103");
+define("IPTC_HEADLINE", "105");
+define("IPTC_CREDIT", "110");
+define("IPTC_SOURCE", "115");
+define("IPTC_COPYRIGHT_STRING", "116");
+define("IPTC_CAPTION", "120");
+define("IPTC_LOCAL_CAPTION", "121");
+
+class IPTC
+{
+    var $meta = [];
+    var $file = null;
+
+    function __construct($filename)
+    {
+        $info = null;
+
+        $size = getimagesize($filename, $info);
+
+        if(isset($info["APP13"])) $this->meta = iptcparse($info["APP13"]);
+
+        $this->file = $filename;
+    }
+
+    function getValue($tag)
+    {
+        return isset($this->meta["2#$tag"]) ? $this->meta["2#$tag"][0] : "";
+    }
+
+    function setValue($tag, $data)
+    {
+        $this->meta["2#$tag"] = [$data];
+
+        $this->write();
+    }
+
+    private function write()
+    {
+        $mode = 0;
+
+        $content = iptcembed($this->binary(), $this->file, $mode);
+
+        $filename = $this->file;
+
+        if(file_exists($this->file)) unlink($this->file);
+
+        $fp = fopen($this->file, "w");
+        fwrite($fp, $content);
+        fclose($fp);
+    }
+
+    private function binary()
+    {
+        $data = "";
+
+        foreach(array_keys($this->meta) as $key)
+        {
+            $tag = str_replace("2#", "", $key);
+            $data .= $this->iptc_maketag(2, $tag, $this->meta[$key][0]);
+        }
+
+        return $data;
+    }
+
+    function iptc_maketag($rec, $data, $value)
+    {
+        $length = strlen($value);
+        $retval = chr(0x1C) . chr($rec) . chr($data);
+
+        if($length < 0x8000)
+        {
+            $retval .= chr($length >> 8) .  chr($length & 0xFF);
+        }
+        else
+        {
+            $retval .= chr(0x80) .
+                       chr(0x04) .
+                       chr(($length >> 24) & 0xFF) .
+                       chr(($length >> 16) & 0xFF) .
+                       chr(($length >> 8) & 0xFF) .
+                       chr($length & 0xFF);
+        }
+
+        return $retval . $value;
+    }
+
+    function dump()
+    {
+        echo "<pre>";
+        print_r($this->meta);
+        echo "</pre>";
+    }
+
+    #requires GD library installed
+    function removeAllTags()
+    {
+        $this->meta = [];
+        $img = imagecreatefromstring(implode(file($this->file)));
+        if(file_exists($this->file)) unlink($this->file);
+        imagejpeg($img, $this->file, 100);
+    }
+}
 
 class Photo extends Module {
 
@@ -902,6 +1028,13 @@ class Photo extends Module {
 			Log::error($this->database, __METHOD__, __LINE__, $this->database->error);
 			return false;
 		}
+				$query	= Database::prepare($this->database, "SELECT url FROM ? WHERE id = '?' LIMIT 1", array(LYCHEE_TABLE_PHOTOS, $this->photoIDs));
+		$photos	= $this->database->query($query);
+		$photo	= $photos->fetch_object();
+		$objIPTC = new IPTC(LYCHEE_UPLOADS_BIG . $photo->url);
+
+		//set keywords
+		$objIPTC->setValue(IPTC_HEADLINE, $title);
 		return true;
 
 	}
@@ -932,6 +1065,13 @@ class Photo extends Module {
 			Log::error($this->database, __METHOD__, __LINE__, $this->database->error);
 			return false;
 		}
+				$query	= Database::prepare($this->database, "SELECT url FROM ? WHERE id = '?' LIMIT 1", array(LYCHEE_TABLE_PHOTOS, $this->photoIDs));
+		$photos	= $this->database->query($query);
+		$photo	= $photos->fetch_object();
+		$objIPTC = new IPTC(LYCHEE_UPLOADS_BIG . $photo->url);
+
+		//set keywords
+		$objIPTC->setValue(IPTC_CAPTION, $description);
 		return true;
 
 	}
@@ -1122,6 +1262,15 @@ class Photo extends Module {
 			Log::error($this->database, __METHOD__, __LINE__, $this->database->error);
 			return false;
 		}
+		$query	= Database::prepare($this->database, "SELECT url FROM ? WHERE id = '?' LIMIT 1", array(LYCHEE_TABLE_PHOTOS, $this->photoIDs));
+		$photos	= $this->database->query($query);
+		$photo	= $photos->fetch_object();
+		$objIPTC = new IPTC(LYCHEE_UPLOADS_BIG . $photo->url);
+
+		//set keywords
+		$objIPTC->setValue(IPTC_KEYWORDS, $tags);
+
+
 		return true;
 
 	}
