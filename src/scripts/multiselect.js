electerious/Lychee

commit 6d4df5f6b7821003a4d18d3df385de389177182d

Introduced album.isSmartID.

src/scripts/multiselect.js
@@ -199,7 +199,7 @@ multiselect.getSelection = function(e) {
 
 			let id = $(this).data('id')
 
-			if (id!=='0' && id!==0 && id!=='f' && id!=='s' && id!=='r' && id!=null) {
+			if (!album.isSmartID(id) && id!=null) {
 
 				ids.push(id)
 				$(this).addClass('active')

src/scripts/album.js
@@ -9,12 +9,18 @@ album = {
 
 }
 
+album.isSmartID = function(id) {
+
+	return id==='0' || id==='f' || id==='s' || id==='r'
+
+}
+
 album.getID = function() {
 
 	let id = null
 
 	let isID = (id) => {
-		if (id==='0' || id==='f' || id==='s' || id==='r') return true
+		if (album.isSmartID(id)) return true
 		return $.isNumeric(id)
 	}
 

src/scripts/contextMenu.js
@@ -47,7 +47,7 @@ contextMenu.album = function(albumID, e) {
 	// fn must call basicContext.close() first,
 	// in order to keep the selection
 
-	if (albumID==='0' || albumID==='f' || albumID==='s' || albumID==='r') return false
+	if (album.isSmartID(albumID)) return false
 
 	// Show merge-item when there's more than one album
 	let showMerge = (albums.json && albums.json.albums && Object.keys(albums.json.albums).length>1)

commit ffef7e3d8fbbbbe58bb311f4f08c85b56b07b028

Fixed bug in multiselect, introduced by 6d4df5f.

src/scripts/multiselect.js
@@ -199,7 +199,7 @@ multiselect.getSelection = function(e) {
 
 			let id = $(this).data('id')
 
-			if (!album.isSmartID(id) && id!=null) {
+			if (id!=null && id!==0 && !album.isSmartID(id)) {
 
 				ids.push(id)
 				$(this).addClass('active')