7ba384e70a0b49efa5096b88ba17a08a6fe400e4
Tobias Reich <tobias.reich.ich@gmail.com>
2016-08-22 14:08:03 +0200

    Updated to minor version

--- php/database/update_030200.php
+++ php/database/update_030200.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Update to version 3.1.3
+ * Update to version 3.2.0
  */
 
 use Lychee\Modules\Database;
@@ -9,21 +9,21 @@ use Lychee\Modules\Response;
 
 // Add parent field to albums
 $query  = Database::prepare($connection, "SELECT `parent` FROM `?` LIMIT 1", array(LYCHEE_TABLE_ALBUMS));
-$result = Database::execute($connection, $query, "update_030103", __LINE__);
+$result = Database::execute($connection, $query, "update_030200", __LINE__);
 
 if ($result===false) {
 
 	$query  = Database::prepare($connection, "ALTER TABLE `?` ADD `parent` BIGINT(14) NOT NULL DEFAULT 0", array(LYCHEE_TABLE_ALBUMS));
-	$result = Database::execute($connection, $query, "update_030103", __LINE__);
+	$result = Database::execute($connection, $query, "update_030200", __LINE__);
 
 	if ($result===false) {
-		Log::error($database, 'update_030103', __LINE__, 'Could not update database (' . $database->error . ')');
+		Log::error($database, 'update_030200', __LINE__, 'Could not update database (' . $database->error . ')');
 		return false;
 	}
 
 }
 
 // Set version
-if (Database::setVersion($connection, '030103')===false) Response::error('Could not update version of database!');
+// if (Database::setVersion($connection, '030200')===false) Response::error('Could not update version of database!');
 
 ?>
--- src/scripts/lychee.js
+++ src/scripts/lychee.js
@@ -6,8 +6,8 @@
 lychee = {
 
 	title           : document.title,
-	version         : '3.1.3',
-	versionCode     : '030103',
+	version         : '3.2.0',
+	versionCode     : '030200',
 
 	updatePath      : '//update.electerious.com/index.json',
 	updateURL       : 'https://github.com/electerious/Lychee',
